7d94642ec643fb6b5c6e3bdf635f36a469a2482f
bbbang105 <2018111366@dgu.ac.kr>
2022-10-02 06:40:00 +0900

    2022-09-22 실습과제#4 3번

---
+++ Ossp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ossp {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("이름을 입력하세요.: ");
+		String name = in.next();
+		System.out.println("학번을 입력하세요.: ");
+		int studentNumber = in.nextInt();
+		
+		System.out.println("<출력>");
+		System.out.println("이름: "+name);
+		System.out.println("학번: "+studentNumber);
+	}
+
+}
